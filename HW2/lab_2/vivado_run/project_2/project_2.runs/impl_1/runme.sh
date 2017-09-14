e9f985d50931212adae7d8c9f85ccae2bcd4be57
zombielinux <will@sutton-family.org>
2017-09-14 15:25:15 -0400

    lab2 vivado runs completed

---
+++ HW2/lab_2/vivado_run/project_2/project_2.runs/impl_1/runme.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# 
+# Vivado(TM)
+# runme.sh: a Vivado-generated Runs Script for UNIX
+# Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+# 
+
+if [ -z "$PATH" ]; then
+  PATH=/tools/xilinx/vivado201504/SDK/2015.4/bin:/tools/xilinx/vivado201504/Vivado/2015.4/ids_lite/ISE/bin/lin64:/tools/xilinx/vivado201504/Vivado/2015.4/bin
+else
+  PATH=/tools/xilinx/vivado201504/SDK/2015.4/bin:/tools/xilinx/vivado201504/Vivado/2015.4/ids_lite/ISE/bin/lin64:/tools/xilinx/vivado201504/Vivado/2015.4/bin:$PATH
+fi
+export PATH
+
+if [ -z "$LD_LIBRARY_PATH" ]; then
+  LD_LIBRARY_PATH=/tools/xilinx/vivado201504/Vivado/2015.4/ids_lite/ISE/lib/lin64
+else
+  LD_LIBRARY_PATH=/tools/xilinx/vivado201504/Vivado/2015.4/ids_lite/ISE/lib/lin64:$LD_LIBRARY_PATH
+fi
+export LD_LIBRARY_PATH
+
+HD_PWD='/nethome/wsutton8/ECE6276/ECE6276/HW2/lab_2/vivado_run/project_2/project_2.runs/impl_1'
+cd "$HD_PWD"
+
+HD_LOG=runme.log
+/bin/touch $HD_LOG
+
+ISEStep="./ISEWrap.sh"
+EAStep()
+{
+     $ISEStep $HD_LOG "$@" >> $HD_LOG 2>&1
+     if [ $? -ne 0 ]
+     then
+         exit
+     fi
+}
+
+# pre-commands:
+/bin/touch .init_design.begin.rst
+EAStep vivado -log barrel_shifter.vdi -applog -m64 -messageDb vivado.pb -mode batch -source barrel_shifter.tcl -notrace
+
+
