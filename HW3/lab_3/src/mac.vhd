f4bf575c271d0fd8dc71b1c57e09ee21b94b0e96
zombielinux <will@sutton-family.org>
2017-09-20 13:16:16 -0400

    Lab3 code working and producing correct output

--- HW3/lab_3/src/mac.vhd
+++ HW3/lab_3/src/mac.vhd
@@ -20,10 +20,12 @@ end mac ;
 architecture mac_arch of mac is
 begin
 	with ctrl_in select
-		mac_unit_out <= a_in+c_in when "00",
-		a_in*b_in when "01",
-		(a_in*b_in)+c_in when "10",
-		'0' when others;
+		mac_unit_out <= std_logic_vector("00000000000000000" + signed(a_in)+signed(c_in)) when "00",
+		std_logic_vector("00000000000000000" + (signed(a_in) * signed(b_in))) when "01",
+		std_logic_vector("00000000000000000" + (signed(a_in) * signed(b_in)) + signed(c_in)) when "10",
+--		"00000000000000001" when "01",
+--		"00000000000000010" when "10", 
+		"00000000000000000" when others;
 
 
 end mac_arch;
