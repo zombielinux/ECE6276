9164296e1cca162877acbc06ae74ca33d16b9e60
zombielinux <will@sutton-family.org>
2017-09-21 19:37:14 -0400

    vivado run completed

--- HW3/lab_3/src/mac_updated.vhd
+++ HW3/lab_3/src/mac_updated.vhd
@@ -20,30 +20,30 @@ end mac ;
 architecture mac_arch of mac is
 	
 	constant mac_out_length : integer := mac_unit_out'length;
-	signal mul_a : std_logic_vector ( 7 downto 0);
-	signal mul_b : std_logic_vector ( 7 downto 0);
-	signal sum_c : std_logic_vector ( 7 downto 0);
+	signal mul_a : signed ( mac_out_length-1 downto 0);
+	signal mul_b : signed ( mac_out_length-1 downto 0);
+	signal sum_c : signed ( mac_out_length-1 downto 0);
 begin
 
 
 
 
 	with ( ctrl_in(0) and ctrl_in(1) ) select
-		mul_a <= "00000000" when '1',
-		a_in when others;
+		mul_a <= (others => '0') when '1',
+		resize(signed(a_in),mac_out_length) when others;
 		
 	with ( ctrl_in(0) xor ctrl_in(1) ) select
-		mul_b <= "00000001" when '0',
-		b_in when others;
+		mul_b <= (0 => '1', others => '0') when '0',
+		resize(signed(b_in),mac_out_length) when others;
 	
  
 	with ctrl_in(0) select	
-		sum_c <= c_in when '0',
-		"00000000" when others;
+		sum_c <= resize(signed(c_in),mac_out_length) when '0',
+		(others => '0') when others;
 		
 		
-	mac_unit_out <= std_logic_vector((resize(resize(signed(mul_a),mac_out_length)*signed(mul_b),mac_out_length))+signed(sum_c));
-
-
+--	mac_unit_out <= std_logic_vector((resize(resize(signed(mul_a),mac_out_length)*signed(mul_b),mac_out_length))+signed(sum_c));
+-- 	mac_unit_out <= std_logic_vector(resize(resize(signed(mul_a),mac_out_length)*resize(signed(mul_b),mac_out_length),mac_out_length)+resize(signed(sum_c),mac_out_length));
+	mac_unit_out <= std_logic_vector(resize(resize(mul_a,mac_out_length)*mul_b,mac_out_length)+ sum_c);
 
 end mac_arch;
