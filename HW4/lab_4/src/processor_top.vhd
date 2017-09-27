b6c02ce0bdd390d75acbb057501f2ffa43dfda35
zombielinux <will@sutton-family.org>
2017-09-27 18:57:26 -0400

    fiddling with i/o of stage1

--- HW4/lab_4/src/processor_top.vhd
+++ HW4/lab_4/src/processor_top.vhd
@@ -23,7 +23,7 @@ architecture processor_top_arch of processor_top is
 begin
 
 --stage1
-ST1 : entity work.stage1 port map (clk,rst_n,en_in_ctrl_in,operand_a_in,operand_b_in,operand_c_in,out_valid_stage1,operand_a_out_stage1);
+ST1 : entity work.stage1 port map (clk,rst_n,en_in,ctrl_in,operand_a_in,operand_b_in,operand_c_in,out_valid_stage1);
 --ST1 : entity work.stage1 port map ();
 --mac
 --MAC : work.mac_updated port map (operand_a_out_stage1);
