5f7e537c2c3724b3972bb9c5624555af123c23d5
zombielinux <will@sutton-family.org>
2017-09-27 18:05:05 -0400

    added entity to instantiation

--- HW4/lab_4/src/processor_top.vhd
+++ HW4/lab_4/src/processor_top.vhd
@@ -20,25 +20,10 @@ end processor_top ;
 
 architecture processor_top_arch of processor_top is
  
-  component stage1 is
-    port(
-        clk,rst_n      : in std_logic  ;
-        en_in          : in std_logic  ; --when en_in = 1, all the inputs are latched
-        ctrl_in        : in std_logic_vector ( 1  downto 0);
-        operand_a_in,operand_b_in,operand_c_in : in std_logic_vector   ( 7  downto 0);
---          out_valid    : out std_logic   ; --when out_valid =1, all the outputs are valid,in all the other cycles, output is invalid
---          result_out   : out std_logic_vector  ( 16 downto 0)
-	out_valid_stage1 : out std_logic;
-	ctrl_in_stage1 : out std_logic_vector ( 1 downto 0);
-	operand_a_out_stage1 : out std_logic_vector ( 7 downto 0);
-	operand_b_out_stage1 : out std_logic_vector ( 7 downto 0);
-	operand_c_out_stage1 : out std_logic_vector ( 7 downto 0);
-  end component
- 
 begin
 
 --stage1
-ST1 : work.stage1 port map (clk,rst_n,en_in_ctrl_in,operand_a_in,operand_b_in,operand_c_in,out_valid_stage1,operand_a_out_stage1);
+ST1 : entity work.stage1 port map (clk,rst_n,en_in_ctrl_in,operand_a_in,operand_b_in,operand_c_in,out_valid_stage1,operand_a_out_stage1);
 --mac
 --MAC : work.mac_updated port map (operand_a_out_stage1);
 --stage2
