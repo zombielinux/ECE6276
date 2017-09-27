ffa5f2cd65d7077b3d0994c4d8bdbe471d38e5bf
zombielinux <will@sutton-family.org>
2017-09-27 12:37:31 -0400

    added variables

--- HW4/lab_4/src/stage1.vhd
+++ HW4/lab_4/src/stage1.vhd
@@ -20,11 +20,18 @@ end processor_top ;
 
 architecture processor_top_arch of processor_top is
 
+	constant operand_length : integer := operand_a'length;
+	out_valid_stage1 : std_logic;
+	ctrl_in_stage1 : std_logic_vector ( 1 downto 0);
+	signal operand_a_in_stage1 : std_logic_vector ( operand_length-1 downto 0);
+	signal operand_b_in_stage1 : std_logic_vector ( operand_length-1 downto 0);
+	signal operand_c_in_stage1 : std_logic_vector ( operand_length-1 downto 0);
+
 begin
 
 --out_valid d-flipflop
 	process (clk, rst_n) begin
-		if (reset = '0') then 
+		if (rst_n = '0') then 
 			out_valid_stage1 <= '0';
 			ctrl_in_stage1 <= (others => '1');
 			operand_a_in_stage1 <= (others => '1');
