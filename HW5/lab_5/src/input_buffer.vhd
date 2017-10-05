41956027c58f3a4646c70634c2e5968db85506e4
zombielinux <will@sutton-family.org>
2017-10-04 23:44:50 -0400

    working on filter_8

--- HW5/lab_5/src/input_buffer.vhd
+++ HW5/lab_5/src/input_buffer.vhd
@@ -53,12 +53,12 @@ begin
 			data_7_out <= (others => '0');
 			
 		elsif (rising_edge(clk)) then
-			if (data_en_buffer < "9") then
+			if (data_en_buffer < X"9") then
 				data_in_en_buff <= '0';
 				data_en_buffer <= std_logic_vector(unsigned(data_en_buffer) + unsigned(data_in_en));
 			else
 				data_in_en_buff <= '1';
-				data_en_buffer = "10";
+				data_en_buffer = X"A";
 			data_0_out <= data_in;
 			data_1_out <= data_0_in;
 			data_2_out <= data_1_in;
