e6786f08f818527b3c7b414c5014a7f44df9c739
zombielinux <will@sutton-family.org>
2017-10-04 22:19:15 -0400

    working on filter_8

--- HW5/lab_5/src/input_buffer.vhd
+++ HW5/lab_5/src/input_buffer.vhd
@@ -37,32 +37,31 @@ architecture input_buffer_arch of input_buffer is
 
 begin
 
---out_valid d-flipflop
-
---	process (clk, rst_n) begin
---		if (rst_n = '0') then 
---			data_0_out <= (others => '0');
---			data_1_out <= (others => '0');
---			data_2_out <= (others => '0');
---			data_3_out <= (others => '0');
---			data_4_out <= (others => '0');
---			data_5_out <= (others => '0');
---			data_6_out <= (others => '0');
---			data_7_out <= (others => '0');
+out_valid d-flipflop
+	process (clk, rst_n) begin
+		if (rst_n = '0') then 
+			data_0_out <= (others => '0');
+			data_1_out <= (others => '0');
+			data_2_out <= (others => '0');
+			data_3_out <= (others => '0');
+			data_4_out <= (others => '0');
+			data_5_out <= (others => '0');
+			data_6_out <= (others => '0');
+			data_7_out <= (others => '0');
 			
---		elsif (rising_edge(clk)) then
---			data_0_out <= data_in;
---			data_1_out <= data_0_in;
---			data_2_out <= data_1_in;
---			data_3_out <= data_2_in;
---			data_4_out <= data_3_in;
---			data_5_out <= data_4_in;
---			data_6_out <= data_5_in;
---			data_7_out <= data_6_in;
---
---	        end if;
+		elsif (rising_edge(clk)) then
+			data_0_out <= data_in;
+			data_1_out <= data_0_in;
+			data_2_out <= data_1_in;
+			data_3_out <= data_2_in;
+			data_4_out <= data_3_in;
+			data_5_out <= data_4_in;
+			data_6_out <= data_5_in;
+			data_7_out <= data_6_in;
 
---	end process;      	        
+	        end if;
+
+	end process;      	        
 
 	        
 end input_buffer_arch;
