bc3b1a7fad8235bc96b5acaef7f1d7cee30bb587
zombielinux <will@sutton-family.org>
2017-10-04 20:19:37 -0400

    working on input buffer

--- HW5/lab_5/src/input_buffer.vhd
+++ HW5/lab_5/src/input_buffer.vhd
@@ -48,7 +48,7 @@ begin
 			data_5_out <= (others => '0');
 			data_6_out <= (others => '0');
 			data_7_out <= (others => '0');
-		elsif and(rising_edge(clk),(data_in_en)) then
+		elsif (rising_edge(clk) and (data_in_en)) then
 			data_0_out <= data_in;
 			data_1_out <= data_0_in;
 			data_2_out <= data_1_in;
