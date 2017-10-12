bb93ee511eeae78020a2749f40f7b090e0dae507
zombielinux <will@sutton-family.org>
2017-10-12 00:05:07 -0400

    trying to fix illegal concurrent statement complex_addsub.vhd

--- HW6/lab_6/src/complex_addsub.vhd
+++ HW6/lab_6/src/complex_addsub.vhd
@@ -33,11 +33,11 @@ begin
 
 	if (op = 0) then
 --in the case of adding	
-		res_real <= a_real+b_real;
-		res_imag <= a_imag+b_imag;
+--		res_real <= a_real+b_real;
+--		res_imag <= a_imag+b_imag;
 	else 
-		res_real <= a_real-b_real;
-		res_imag <= a_imag-b_imag;
+--		res_real <= a_real-b_real;
+--		res_imag <= a_imag-b_imag;
 	end if;
 
 
